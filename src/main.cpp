148a5645969315de3b8657bbea159cae9389819a
Davide Cristini <davcri91@gmail.com>
2021-07-16 20:12:12 +0200

    Use math library in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include "FuocoConfig.h"
+#include "../libs/math/math.h"
 
 int main()
 {
     std::cout << "Fuoco " << Fuoco_VERSION_MAJOR << "." << Fuoco_VERSION_MINOR << std::endl;
+    std::cout << sqrt(16) << std::endl;
     return 0;
 }
