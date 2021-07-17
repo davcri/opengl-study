3aaf09bbc3a360c893daa40f9383febb99e8dbc6
Davide Cristini <davcri91@gmail.com>
2021-07-17 12:24:05 +0200

    Fix CMake file and import GLFW in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,9 @@
 #include <iostream>
 #include "FuocoConfig.h"
-
-#ifdef USE_MYMATH
-#include "../libs/math/math.h"
-#else
-#include <math.h>
-#endif
+#include <GLFW/glfw3.h>
 
 int main()
 {
     std::cout << "Fuoco " << Fuoco_VERSION_MAJOR << "." << Fuoco_VERSION_MINOR << std::endl;
-#ifdef USE_MYMATH
-    std::cout << "Using custom math" << std::endl;
-    const double outputValue = mysqrt(16);
-#else
-    std::cout << "Using math.h" << std::endl;
-    const double outputValue = sqrt(16);
-#endif
-    std::cout << outputValue << std::endl;
     return 0;
 }
