119e765b428aa5215265db26371cf1d7fbd4866d
Davide Cristini <davcri91@gmail.com>
2021-08-17 19:24:38 +0200

    Draw first triangle

--- src/main.cpp
+++ src/main.cpp
@@ -42,6 +42,7 @@ int main()
 
   glViewport(0, 0, 800, 600);
   glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+  // framebufferSizeCallback(window, 800, 600);
 
   float vertices[] = {
       -0.5f, -0.5f, 0.0f,
@@ -74,12 +75,58 @@ int main()
   glGenBuffers(1, &VBO);
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+
+  unsigned int VAO;
+  glGenVertexArrays(1, &VAO);
+  // 1. bind Vertex Array Object
+  glBindVertexArray(VAO);
+  // 2. copy our vertices array in a buffer for OpenGL to use
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  // 3. then set our vertex attributes pointers
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+
+  // fragment shader
+  unsigned int fragmentShader;
+  char *fragmentSource = "#version 330 core\n"
+                         "out vec4 FragColor;\n"
+                         "void main() {\n"
+                         "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                         "}\0";
+  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+  glCompileShader(fragmentShader);
+
+  unsigned int shaderProgram;
+  shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+  if (!success)
+  {
+    glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+  }
+  // glUseProgram(shaderProgram);
+
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
 
   while (!glfwWindowShouldClose(window))
   {
     processInput(window);
     glClearColor(0.2, 0.2, 0.3, 1);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    // 4. draw the object
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    // draw 
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
