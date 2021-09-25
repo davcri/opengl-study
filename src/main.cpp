b2753f2f5dca144bebccb07a7cecadce6763da59
Davide Cristini <davcri91@gmail.com>
2021-09-25 17:28:29 +0200

    Add stb_image and prepare code for Textures chapter

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
-#include "FuocoConfig.h"
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include "FuocoConfig.h"
 #include "shader.h"
 #include "cmath"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 void framebufferSizeCallback(GLFWwindow *window, int width, int height)
 {
@@ -47,11 +49,14 @@ int main()
   // framebufferSizeCallback(window, 800, 600);
 
   float vertices[] = {
-      0.0f, 0.5f, 0.0f, 1.0f, 0.0, 0.0,
-      0.5f, -0.5f, 0.0f, 0.0f, 1.0, 0.0,
-      -0.5f, -0.5f, 0.0f, 0.0f, 0.0, 1.0,
-      //
-  };
+      0.5f, 0.5f, 0.0f,
+      -0.5f, 0.5f, 0.0f,
+      0.5f, -0.5f, 0.0f,
+      -0.5f, -0.5f, 0.0f};
+
+  unsigned int indices[] = {
+      0, 1, 2,
+      1, 2, 3};
 
   unsigned int VAO; // vertex array object
   glGenVertexArrays(1, &VAO);
@@ -62,24 +67,35 @@ int main()
   glGenBuffers(1, VBO);
   glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
 
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
-  glEnableVertexAttribArray(1);
-
-  unsigned int VAO2;
-  glGenVertexArrays(1, &VAO2);
-  glBindVertexArray(VAO2);
-
-  glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
-  // glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)(9 * sizeof(float)));
-  glEnableVertexAttribArray(0);
+  unsigned int EBO;
+  glGenBuffers(1, &EBO);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
   Shader program("./shaders/shader.vert", "./shaders/shader.frag");
 
   glPolygonMode(GL_FRONT_AND_BACK, GL_TRIANGLES);
+
+  // loading texture
+  int width, height, nrChannels;
+  unsigned char *data = stbi_load("./assets/Preview.png", &width, &height, &nrChannels, 0);
+
+  unsigned int texture;
+  glGenTextures(1, &texture);
+  glBindTexture(GL_TEXTURE_2D, texture);
+  if (data)
+  {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    stbi_image_free(data);
+  }
+  else
+  {
+    std::cout << "ERROR::Loading texture failed" << std::endl;
+    return -1;
+  }
 
   while (!glfwWindowShouldClose(window))
   {
@@ -92,7 +108,7 @@ int main()
     program.use();
 
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
--- src/shaders/shader.vert
+++ src/shaders/shader.vert
@@ -1,7 +1,6 @@
 #version 330 core
 
 layout(location = 0) in vec3 aPos;
-layout(location = 1) in vec3 aCol;
 
 out vec3 outCol;
 
